58dd83fafa2dbf071c3f4d6281d97fb50bc1387a
Paul Sutcliffe <frpaulas@pop-os.localdomain>
2025-10-24 09:54:03 -0400

    Fix line component text overflow alignment when indented

--- src/lib/page_helpers/line.svelte
+++ src/lib/page_helpers/line.svelte
@@ -2,7 +2,7 @@
 	let { text, indent = false, bold = false, amen = false } = $props();
 	let this_class = $state('');
 	if (indent) {
-		this_class += ' indent-4';
+		this_class += ' pl-4';
 	}
 	if (bold) {
 		this_class += ' font-bold';
