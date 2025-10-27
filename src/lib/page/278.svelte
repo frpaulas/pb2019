aa2d64c5f0f4b9d3728b8a7f8d944e3a1fb34d98
Paul Sutcliffe <frpaulas@pop-os.localdomain>
2025-10-27 13:35:44 -0400

    Add psalter pages 279-467 (odd numbers only)
    - Created 95 page files (279, 281, 283, ... 467) from template
    - Updated PAGE_ORDER to include all new pages
    - Pages ready for psalm content to be added

---
+++ src/lib/page/278.svelte
@@ -0,0 +1,19 @@
+<script>
+	import PageNumber from '$lib/page_helpers/page_number.svelte';
+	import PsalmNumber from '$lib/page_helpers/psalm_number.svelte';
+	import SectionTitle from '$lib/page_helpers/section_title.svelte';
+	import ShowPsalm from '$lib/page_helpers/show_psalm.svelte';
+	import { getPsalmMeta } from '$lib/db/psalms';
+
+	const psalm7 = getPsalmMeta(7);
+	const psalm8 = getPsalmMeta(8);
+</script>
+
+<ShowPsalm ps={7} from={15} to={18} />
+
+<PsalmNumber n={psalm8.number} />
+<SectionTitle fancy text={psalm8.name} latin_size />
+
+<ShowPsalm ps={8} />
+
+<PageNumber page={276} text="the psalter" />
